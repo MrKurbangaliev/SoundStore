0cc7dcfd3cb2133038d6783dd58e21c88d1a527d
Danat Kurbangaliev <kurbangalievdanat@gmail.com>
2021-11-12 19:18:49 +0200

    Make code more concise

--- app/src/main/java/com/example/soundstore/ui/store/StoreFragment.java
+++ app/src/main/java/com/example/soundstore/ui/store/StoreFragment.java
@@ -7,33 +7,24 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.soundstore.databinding.FragmentStoreBinding;
 
 public class StoreFragment extends Fragment {
 
-    private StoreViewModel storeViewModel;
     private FragmentStoreBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        storeViewModel =
-                new ViewModelProvider(this).get(StoreViewModel.class);
+        StoreViewModel storeViewModel = new ViewModelProvider(this).get(StoreViewModel.class);
 
         binding = FragmentStoreBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textStore;
-        storeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        storeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
